b309a9ea11995cca500a726a4e8bf4674d2e348c
R.J. Steinert (rjstatic) <rj@rjstatic.me>
2011-06-12 18:41:26 -0400

    Only full chart objects accepted in url now

--- index.js
+++ index.js
@@ -37,21 +37,13 @@ var createHighchartsWindow = function(fn) {
 };
 
 this.server = http.createServer(function(request, response) {
-	console.log("request");
-	var url 		= parse(request.url, true),
-		chartTypeMatch 	= /^\/(\w+)$/.exec(url.pathname),
-		chartType	= chartTypeMatch ? chartTypeMatch[1] : null,
-		query		= (url.query || {}),
-		width		= query.width || 640,
-		height		= query.height || 480,
-		data		= [];
-    custom  = query.custom;    
-	if (query.data) {
-		map(query.data.split(','), function(i) {
-			data.push(parseFloat(this));
-		});
-	}
-	createHighchartsWindow(function(window) {
+	var url = parse(request.url, true),
+	  query = (url.query || {}),
+    chartDefinition  = query.chart;    
+  if(!chartDefinition) {
+   return;
+  }
+  createHighchartsWindow(function(window) {
 		var $	= window.jQuery,
 			Highcharts 	= window.Highcharts,
 			document	= window.document,
@@ -59,46 +51,31 @@ this.server = http.createServer(function(request, response) {
 			chart, svg, convert;
 		
 		$container.appendTo(document.body);
-    // Generate a chart from a custom JSON object 
-    if(custom) {
-      customChart = $.parseJSON(custom);
-      customChart.chart.renderTo = $container[0];
-      customChart.chart.renderer = 'SVG';
-      chart = new Highcharts.Chart(customChart);
-    }
-    // Generate a chart from data provided
-    if(data.length > 0) {	
-      chart = new Highcharts.Chart({
-        chart: {
-          defaultSeriesType: chartType,
-          renderTo: $container[0],
-          renderer: 'SVG',
-          width: width,
-          height: height
-        },
-        series: [{
-          data: data
-        }]
-      });
-	  }	
+    customChart = $.parseJSON(chartDefinition);
+    customChart.chart.renderTo = $container[0];
+    customChart.chart.renderer = 'SVG';
+    chart = new Highcharts.Chart(customChart);
+    
     svg = $container.children().html();
-  	// Generate SVG - just for debugging 
-    //fs.writeFile('chart.svg', svg, function() { console.log('done'); });	
+  	
+    // Generate SVG - just for debugging 
+    fs.writeFile('chart.svg', svg, function() { console.log('done'); });	
 		// Start convert
-		convert	= spawn('convert', ['svg:-', 'png:-']);
+		if(svg) {
+      convert	= spawn('convert', ['svg:-', 'png:-']);
 
-		// We're writing an image, hopefully...
-		response.writeHeader(200, {'Content-Type': 'image/png'});
-		
-		// Pump in the svg content
-		console.log(svg);
-    convert.stdin.write(svg);
-		convert.stdin.end();
-		
-		// Write the output of convert straight to the response
-		convert.stdout.on('data', function(data) {
-			response.write(data);
-		});
+      // We're writing an image, hopefully...
+      response.writeHeader(200, {'Content-Type': 'image/png'});
+      
+      // Pump in the svg content
+      convert.stdin.write(svg);
+      convert.stdin.end();
+      
+      // Write the output of convert straight to the response
+      convert.stdout.on('data', function(data) {
+        response.write(data);
+      });
+    }
 
 		// When we're done, we're done
 		convert.on('exit', function(code) {
